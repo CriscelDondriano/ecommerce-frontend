6da5591d21355e1e95e9e5c5eefb65ff7130eb64
CriscelDondriano <dondrianocriscel31@gmail.com>
2024-12-09 07:30:44 +0800

    Register ShowPassword

--- src/components/Register.js
+++ src/components/Register.js
@@ -91,7 +91,7 @@ const Register = () => {
         });
 
         console.log("Registration successful:", response.data);
-        navigate("/login");
+        navigate("/");
       } catch (error) {
         if (error.response && error.response.data.errors) {
           setErrors(error.response.data.errors);
